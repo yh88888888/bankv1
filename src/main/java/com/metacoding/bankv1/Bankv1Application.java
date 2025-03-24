a0c04abbe9edc66360765226a768bf7a3408992a
getinthere <getinthere@naver.com>
2025-03-24 10:02:23 +0900

    start

---
+++ src/main/java/com/metacoding/bankv1/Bankv1Application.java
@@ -0,0 +1,13 @@
+package com.metacoding.bankv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bankv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bankv1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/metacoding/bankv1/Bankv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metacoding.bankv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bankv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

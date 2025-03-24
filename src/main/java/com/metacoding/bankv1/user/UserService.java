c90b9646a28bb5abd339e04d56900ad61f53417a
yh88888888 <yh88888888jps@gmail.com>
2025-03-24 21:17:34 +0900

    bankv1

--- src/main/java/com/metacoding/bankv1/user/UserService.java
+++ src/main/java/com/metacoding/bankv1/user/UserService.java
@@ -1,5 +1,6 @@
 package com.metacoding.bankv1.user;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,12 +9,15 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
 
+    @Transactional
     public void 회원가입(UserRequest.JoinDTO joinDTO) {
 //1. 동일 유저네임 있는지 검사
         User user = userRepository.findByUsername(joinDTO.getUsername());
         //2. 있으면, exception 터트리기
         //오류는 바로 터뜨려야 한다.
-        if (user != null) {throw new RuntimeException("동일한 username이 존재합니다.");}
+        if (user != null) {
+            throw new RuntimeException("동일한 username이 존재합니다.");
+        }
         //3. 없으면 회원가입하기
 //        String encPassword = 해시암호화라이브러리(joinDTO.getPassword());
 
